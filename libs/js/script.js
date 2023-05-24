b549ed8a3f034ddddcf8f0d19257284d73788f8a
Anara <berdali.aygerim9@gmail.com>
2023-05-24 13:20:39 +0100

    modals added; location/departments dropdowns populated

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,14 +1,37 @@
 'use strict';
 
+$('#search').on('keyup', function (){
+    const searchValue = $(this).val();
+
+    $.ajax({
+        method: 'POST',
+        url: 'libs/php/search.php',
+        data: {
+            department: $('#selectDepartment').val(),
+            location: $('#selectLocation').val(),
+            search_value: searchValue
+        },
+        success: function(result){
+
+        },
+        error: function(error) {
+            console.error(error);
+        }
+    })
+})
+
+
+
 $.ajax({
     method: 'GET',
     url: 'libs/php/getAll.php',
     success: function(result) {
         console.log(result);
         const data = result.data;
-        data.forEach(element => {
-           populateEmployees(element); 
-        });
+        populateEmployeesTab(data); 
+   
+        const employeesNumber = data.length;
+        console.log(employeesNumber);
     },
     error: function(error) {
         console.error(error);
@@ -16,21 +39,25 @@ $.ajax({
   
 });
 
-function populateEmployees(element) {
-    $('#employeesList').append(`
-        <tr>
-            <td>${element.firstName}</td>
-            <td>${element.lastName}</td>
-            <td>${element.email}</td>
-            <td>${element.department}</td>
-            <td>${element.location}</td>
-            <td><button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editEmployee">edit</button></td>
-            <td><button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteEmployee">delete</button></td>
+function populateEmployeesTab(data) {
+    data.forEach(function(val, i, arr) {
+        $('#employeesList').append(`
+        <tr data-employee-record='${JSON.stringify(val)}'>
+            <td>${++i}</td>
+            <td>${val.firstName}</td>
+            <td>${val.lastName}</td>
+            <td><button class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#employeeDetails">Details</button></td>
+            <td><button class="btn btn-sm btn-warning editEmployee" data-bs-toggle="modal" data-bs-target="#editEmployee">edit</button></td>
+            <td><button class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#deleteEmployee">delete</button></td>
+        
         </tr>
          
         
     `);
+    });
+   
 }
+
 
 $.ajax({
     method: 'GET',
@@ -38,9 +65,10 @@ $.ajax({
     success: function(result) {
         console.log(result);
         const data = result.data;
-        data.forEach(element => {
-           populateDepartments(element); 
-        });
+        populateDepartmentsDropdown(data); 
+        populateDepartmentsTab(data); 
+      
+          
     },
     error: function(error) {
         console.error(error);
@@ -48,15 +76,112 @@ $.ajax({
   
 });
 
-function populateDepartments(element) {
-    $('#departmentsList').append(`
-        <tr>
-            <th scope="row">${element.id}</th>
-            <td>${element.name}</td>
-            <td>${element.locationID}</td>
-            <td><button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editEmployee">edit</button></td>
-        </tr>
-         
-        
-    `);
+function populateDepartmentsDropdown(data) {
+
+    data.forEach(function (val, i, arr) {
+        $('#selectDepartment').append($('<option>', {
+        value: val.id,
+        text: val.name
+        }));
+    });
+
 }
+     
+function populateDepartmentsTab(data) {
+    data.forEach(function(val, i, arr) {
+        $('#departmentsList').append(`
+        <tr>
+            <td>${val.name}</td>
+            <td><button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editEmployee">edit</button></td>
+            <td><button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteEmployee">delete</button></td>
+        </tr>       
+    `);
+    })
+    
+}
+    
+    
+
+
+$.ajax({
+    method: 'GET',
+    url: 'libs/php/getAllLocations.php',
+    success: function(result) {
+        console.log(result);
+        const data = result.data;
+        populateLocationsTab(data); 
+        populateLocationsDropdown(data);
+     
+          
+    },
+    error: function(error) {
+        console.error(error);
+    }
+  
+});
+
+function populateLocationsTab(data) {
+    data.forEach(function(val, i, arr) {
+        $('#locationsList').append(`
+        <tr>
+            <td>${val.name}</td>
+            <td><button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editEmployee">edit</button></td>
+            <td><button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteEmployee">delete</button></td>
+        </tr> 
+    `);
+
+ })
+    
+}
+
+function populateLocationsDropdown(data) {
+
+    data.forEach(function (val, i, arr) {
+        $('#selectLocation').append($('<option>', {
+        value: val.id,
+        text: val.name
+        }));
+    });
+
+}
+
+
+// Form validation
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  function runMe(){
+
+    'use strict'
+  const forms = document.querySelectorAll('.validated-form')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+}
+
+$(document).ready(function($) {
+    $('.editEmployee').on('click', function() {
+        const employeeDetails = JSON.parse($(this).closest('tr').attr('data-employee-record'));
+    console.log(employeeDetails);
+    if(employeeDetails){
+        // $('#editEmployee').empty();
+
+     $('#firstNameInput').val(employeeDetails.firstName);
+     $('#lastNameInput').val(employeeDetails.lastName);
+     $('#emailInput').val(employeeDetails.email);
+     $('#departmentInput').val(employeeDetails.department);
+     $('#locationInput').val(employeeDetails.location);
+    }
+});
+})
+
+
+
