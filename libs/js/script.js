97e651cca922fa0b1ce0d973e7cd07164b7ff177
Anara <berdali.aygerim9@gmail.com>
2023-06-08 12:15:17 +0100

    modals look update; deleteDepartment and deleteLocation features

--- libs/js/script.js
+++ libs/js/script.js
@@ -50,15 +50,28 @@ function populateEmployeesTab(data) {
     const $employeesList =  $('#employeesList');
     $employeesList.empty();
 
+        // const name = ${val.firstName}
+        // const surname = ${val.lastName}
+        // const fullName = name.concat(',' surname);
+
     data.forEach(function(val, i, arr) {
         $employeesList.append(`
+        
         <tr data-employee-record='${JSON.stringify(val)}'>
-            <td>${++i}</td>
             <td>${val.firstName}</td>
             <td>${val.lastName}</td>
-            <td><button class="btn btn-sm btn-primary viewEmployeeDetails" data-bs-toggle="modal" data-bs-target="#view">Details</button></td>
-            <td><button class="btn btn-sm btn-warning editEmployee" data-bs-toggle="modal" data-bs-target="#employeeDetails">Edit</button></td>
-            <td><button class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#deleteInfo">Delete</button></td>
+            <td>${val.department}</td>
+            <td>${val.location}</td>
+
+            <td><button class="btn btn-sm btn-info viewEmployeeDetails" data-bs-toggle="modal" data-bs-target="#view">
+            <i class="fa fa-eye" aria-hidden="true"></i></button></td>
+
+            <td><button class="btn btn-sm btn-warning" data-bs-toggle="modal" data-bs-target="#employeeDetails" data-id=${val.id}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+            </svg></button></td>
+
+            <td><button class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#deleteEmployee"> <i class="fa fa-trash" aria-hidden="true"></i></button></td>
         
         </tr>
          
@@ -80,7 +93,7 @@ $(document).on('click', '.viewEmployeeDetails', function() {
       $('#employeeName').html(employeeName);
       $('#email').html(employee.email);
       $('#jobTitle').html(employee.jobTitle);
-      $('#department').html(employee.department);
+      $('#viewDepartment').html(employee.department);
       $('#location').html(employee.location);
  
     }
@@ -88,32 +101,95 @@ $(document).on('click', '.viewEmployeeDetails', function() {
 
 //////////// EDIT EMPLOYEE DETAILS modal ////////////////
 
-// Event binding on dynamically created elements:
-$(document).on('click', '.editEmployee', function() {
-    $('#employeeDetailsAction').html('Edit Employee Details');
-    $('#submitEmployeeDetails').html('Update information');
-    $('#employeeAlert').addClass('d-none');
-    const employeeDetails = JSON.parse($(this).closest('tr').attr('data-employee-record'));
-    if (employeeDetails) {
-      $('#employeeIDInput').val(employeeDetails.id);
-      $('#firstNameInput').val(employeeDetails.firstName);
-      $('#lastNameInput').val(employeeDetails.lastName);
-      $('#emailInput').val(employeeDetails.email);
-      $('#jobTitleInput').val(employeeDetails.jobTitle);
-      // Set employee's department:
-      $('#departmentSelect > option').each(function(i, elem) {
-          if ($(elem).text() === employeeDetails.department) {
-              $(elem).attr('selected', true);
-          }
-      });
-        // Set employee's location:
-        $('#locationSelect > option').each(function(i, elem) {
-        if ($(elem).text() === employeeDetails.location) {
-            $(elem).attr('selected', true);
-        }
-    });
+$('#employeeDetails').on('show.bs.modal', function (e) {
+
+    $.ajax({
+      url: "libs/php/getPersonnelByID.php",
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        id: $(e.relatedTarget).attr('data-id') // Retrieves the data-id attribute from the calling button
+      },
+      success: function (result) {
+            
+      var resultCode = result.status.code
+
+      if (resultCode == 200) {
+
+        // Update the hidden input with the employee id so that
+        // it can be referenced when the form is submitted
+        
+        $('#employeeID').val(result.data.personnel[0].id);
+        
+        $('#firstName').val(result.data.personnel[0].firstName);
+        $('#lastName').val(result.data.personnel[0].lastName);
+        $('#jobTitle').val(result.data.personnel[0].jobTitle);
+        $('#emailAddress').val(result.data.personnel[0].email);
+        
+        $('#department').html("");
+                
+        $.each(result.data.department, function () {
+          
+					$('#department').append($("<option>", {
+						value: this.id,
+						text: this.name
+					})); 	
+          
+        })
+        
+      } else {
+
+        $('#employeeDetails .modal-title').replaceWith("Error retrieving data");
+
+      } 
+
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      $('#employeeDetails.modal-title').replaceWith("Error retrieving data");
     }
+  });
+
+})
+
+// Executes when the form button with type="submit" is clicked
+
+$('#employeeForm').on("submit", function(e) {
+  
+  // stop the default browser behviour
+  
+  e.preventDefault();
+  
+  // AJAX call
+  
+  
+})
+
+// The "shown" event triggers after the modal appears
+// and can be used to run commands that won't work on hidden elements
+
+$('#employeeDetails').on('shown.bs.modal', function () {
+  
+  // You may optionally use the the following command to place 
+  // the cursor in the first input as a courtesy to the user.
+  // Commands like this that manipulate the state of a control
+  // will only work once it is visible which is why it is in
+  // the "shown" event handler
+  
+  $('#firstName').focus();
+  
 });
+
+// The "hide" and "hidden" events trigger before and after
+// the modal disappears and can be used to clear down the form.
+// This is useful if the form needs to be empty the next time 
+// that it is shown
+
+// $('#employeeDetails').on('hidden.bs.modal', function () {
+  
+//   $('#employeeForm')[0].reset();
+  
+// });
+
 
 //////////// ADD EMPLOYEE modal ////////////////
 
@@ -201,8 +277,11 @@ function populateDepartmentsTab(data) {
         $('#departmentsList').append(`
         <tr data-location-record='${JSON.stringify(val)}'>
             <td>${val.name}</td>
-            <td><button class="btn btn-warning editDepartment" data-bs-toggle="modal" data-bs-target="#departmentDetails">edit</button></td>
-            <td><button class="btn btn-danger deleteDepartment" data-bs-toggle="modal" data-bs-target="#deleteInfo">delete</button></td>
+            <td><button class="btn btn-sm btn-warning editDepartment" data-bs-toggle="modal" data-bs-target="#departmentDetails"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+          </svg></button></td>
+
+            <td><button class="btn btn-sm btn-danger deleteInfo"" data-bs-toggle="modal" data-bs-target="#deleteDepartment" data-id=${val.id}><i class="fa fa-trash" aria-hidden="true"></i></button></td>
         </tr>       
     `);
     })
@@ -268,23 +347,67 @@ $('#submitDepartment').on('click', function() {
 
 //////////////////////// DELETE DEPARTMENT modal //////////////////////
 
-$('#confirmToDelete').on('click', function() {
+// $('#confirmToDelete').on('click', function() {
+//     $.ajax({
+//         method: 'POST',
+//         url: 'libs/php/deleteDepartmentByID.php',
+//         data: {
+//             department_id:$('#departmentIDInput').val()
+//         },
+//         success: function(result){
+//             console.log('delete', result);
+//         },
+//         error: function(error){
+//             console.error(error);
+//         }
+//     })
+
+// })
+
+
+$("#deleteDepartment").click(function() {
+   
     $.ajax({
-        method: 'POST',
-        url: 'libs/php/deleteDepartmentByID.php',
-        data: {
-            department_id:$('#departmentIDInput').val()
-        },
-        success: function(result){
-            console.log('delete', result);
-        },
-        error: function(error){
-            console.error(error);
-        }
-    })
-
-})
-
+      url: "libs/php/checkDepartmentUse.php",
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        id: $(this).attr("data-id") // Retrieves the data-id attribute from the calling button
+      },
+      success: function (result) {
+        console.log(result);
+                   
+        if (result.status.code == 200) {
+  
+          if (result.data[0].departmentCount == 0) {
+            
+            $("#departmentName").text(result.data[0].departmentName);
+  
+            $('#deleteDepartment').modal("show");
+            
+          } else {
+                              
+            $("#cantDeleteDeptName").text(result.data[0].departmentName);
+            $("#pc").text(result.data[0].departmentCount);
+            
+            $('#cantDeleteDepartmentModal').modal("show");          
+            
+          }
+          
+        } else {
+  
+          $('#exampleModal .modal-title').replaceWith("Error retrieving data");
+  
+        } 
+  
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        $('#exampleModal .modal-title').replaceWith("Error retrieving data");
+      }
+    });
+                             
+  });
+  
 
 
 
@@ -301,6 +424,7 @@ function getAllLocations(){
         url: 'libs/php/getAllLocations.php',
         success: function(result) {
             const data = result.data;
+            console.log(data);
             populateLocationsTab(data); 
             populateLocationsDropdown(data);
         
@@ -318,8 +442,13 @@ function populateLocationsTab(data) {
         $('#locationsList').append(`
         <tr data-city='${JSON.stringify(val)}'>
             <td>${val.name}</td>
-            <td><button class="btn btn-warning editLocation" data-bs-toggle="modal" data-bs-target="#locationDetails">edit</button></td>
-            <td><button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteInfo">delete</button></td>
+            <td><button class="btn btn-sm btn-warning editLocation" data-bs-toggle="modal" data-bs-target="#locationDetails"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+          </svg></button></td>
+            <td><button class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#deleteInfo"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+          </svg></button></td>
         </tr> 
     `);
 
@@ -354,7 +483,7 @@ const city = JSON.parse($(this).closest('tr').attr('data-city'));
 
 //////////////////////// ADD LOCATION modal //////////////////////
 
-$('#addLocation').on('click', function(){
+$('#locationDetails').on('click', function(){
     $('#locationInput').val('');
     $('#locationDetailsAction').html('Location Details');
     $('#submitLocation').html('Add new location');
@@ -388,25 +517,13 @@ $('#submitLocation').on('click', function() {
 
 // ---------------------------------------------------------FORM VALIDATION section----------------------------------------------------------//
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  function runMe(){
+  
 
-    'use strict'
-  const forms = document.querySelectorAll('.validated-form')
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
 
-      form.classList.add('was-validated')
-    }, false)
-  })
-}   
-    
+
+
+
 
 
 
